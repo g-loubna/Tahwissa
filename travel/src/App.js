3ab3a6da08e90a902efaa60c49e3e615a3778244
AyoubKhalfi23 <123749255+AyoubKhalfi23@users.noreply.github.com>
2024-01-04 21:29:13 +0100

    Update App.js

--- travel/src/App.js
+++ travel/src/App.js
@@ -15,13 +15,13 @@ import PlusDeDetailsPlace from "./Components/Places/DetailPlace/PlusDeDetailPlac
 import PlusDeDetailsArticle from "./Components/Article/PlusDeDetailsArticle/PlusDeDetailsArticle";
 import LoadMoreRestaurant from "./Components/Restaurants/LoadMoreRestaurant";
 import Profile from "./Components/Profile";
-import SignInPage from './components/SignInPageComponent/SignInPageComponent.js';
-import SignUpPage from './components/SignUpPageComponent/SignUpPageComponent.js';
-import PostComponent from './components/postComponent/postComponent.js';
-import DetailedPostComponent from './components/detailedPostComponent/detailedPostComponent.js';
-import FreindPostsPage from './components/freindPostsPageComponent/freindPostsPage.js';
-import CommentsPage from './components/commentsPageComponent/commentsPageComponent.js';
-import ResetPassword from './components/resetPasswordComoponent/resetPasswordComponent.js'; 
+import SignInPage from './Components/SignInPageComponent/SignInPageComponent.js';
+import SignUpPage from './Components/SignUpPageComponent/SignUpPageComponent.js';
+import PostComponent from './Components/postComponent/postComponent.js';
+import DetailedPostComponent from './Components/detailedPostComponent/detailedPostComponent.js';
+import FreindPostsPage from './Components/freindPostsPageComponent/freindPostsPage.js';
+import CommentsPage from './Components/commentsPageComponent/commentsPageComponent.js';
+import ResetPassword from './Components/resetPasswordComoponent/resetPasswordComponent.js'; 
 const App = () => {
  return(
   
@@ -33,8 +33,14 @@ const App = () => {
        <Route path='/details' element={<RestaurantDetails/>}/>
        <Route path='/detailsarticle' element={<PlusDeDetailsArticle/>}/>
        <Route path="/loadmore" element={<LoadMoreRestaurant/>} /> */}
-        <Route path="/" element={<Profile/>} /> 
-
+       <Route path="/" element={<Profile/>} /> 
+       <Route path="/SignInPage"  element={<SignInPage/>} />
+       <Route path="/ResetPassword"  element={<ResetPassword/>} />
+       <Route path="/SignUpPage"  element={<SignUpPage/>} />
+       <Route path="/PostComponent"  element={<PostComponent/>} />
+       <Route path="/DetailedPostComponent"  element={<DetailedPostComponent/>} />
+       <Route path="/FreindPostsPage"  element={<FreindPostsPage/>} />
+       <Route path="/CommentsPage"  element={<CommentsPage/>} />
 
       </Routes>
    </Router>
